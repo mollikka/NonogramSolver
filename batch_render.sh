7bd04494f04878ab0bdb32968100275028a96314
Lauri Tervonen <lauri.tervonen@reaktor.com>
2025-09-02 22:17:19 +0300

    Convert animations to webm

--- batch_render.sh
+++ batch_render.sh
@@ -1,6 +1,9 @@
 python3 draw.py
 cd renders
 for f in *.gif; do
-    ffmpeg -i "$f" -c:v libwebp -lossless 0 -qscale 50 -preset picture -loop 0 -an "${f%.gif}.webp"
+    ffmpeg -i "$f" -c:v libvpx-vp9 -b:v 0 -crf 30 \
+        -pix_fmt yuv420p -row-mt 1 \
+        -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
+        "${f%.gif}.webm"
 done
 cd ..
